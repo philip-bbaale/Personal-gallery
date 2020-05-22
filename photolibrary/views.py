9efd2fdf7f2861a599899698352ea9d6b9897200
Pip Bbaale <namugera.philip@gmail.com>
2020-05-22 15:48:10 +0300

    1.adding pictures index 2.adding pictures detail 3.adding pictures category 4.adding pictures locatiton 5.adding pictures search results

--- photolibrary/views.py
+++ photolibrary/views.py
@@ -46,4 +46,4 @@ def search_results(request):
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})<!---->
+        return render(request, 'search.html',{"message":message})
