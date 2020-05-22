4c5ddd75408bd88e09e5ef0e844701b0860d0fa6
Pip Bbaale <namugera.philip@gmail.com>
2020-05-22 19:19:08 +0300

    changing location and category view

--- photolibrary/urls.py
+++ photolibrary/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', views.pictures_index, name='pictures_index'),
     url(r'^search/', views.search_results, name='search_results'),
     path('<int:pk>/', views.pictures_detail, name='pictures_detail'),
-    path('<category>/', views.pictures_category, name="pictures_category"),
     path('<location>/', views.pictures_location, name="pictures_location"),
+    path('<category>/', views.pictures_category, name="pictures_category"),
 ]
 
 if settings.DEBUG:
